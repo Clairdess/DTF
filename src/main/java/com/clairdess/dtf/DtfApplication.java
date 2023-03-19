4345186fc60a13991ef2bf4d7e7d3c7dee975dc2
Daniil Davydov <itsclairdess@gmail.com>
2023-03-19 19:28:54 +0900

    Initial commit

---
+++ src/main/java/com/clairdess/dtf/DtfApplication.java
@@ -0,0 +1,13 @@
+package com.clairdess.dtf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtfApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/clairdess/dtf/model/Comment.java
@@ -0,0 +1,4 @@
+package com.clairdess.dtf.model;
+
+public class Comment {
+}
